11cda10149ff5b4460aad40a09a6c7f8d20215b3
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2012-08-27 01:10:31 -0300

    Adding gui button

--- script/compile.sh
+++ script/compile.sh
@@ -3,4 +3,6 @@ cd ~/sbking/src/core
 javac *.java -d ~/sbking/src/bin -classpath ~/sbking/src
 cd ~/sbking/src/gui/sample
 javac *.java -d ~/sbking/src/bin -classpath ~/sbking/src
+cd ~/sbking/src/gui
+javac *.java -d ~/sbking/src/bin -classpath ~/sbking/src
 cd ~/sbking
---
+++ src/gui/CardButton.java
@@ -0,0 +1,22 @@
+import core.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+import java.net.URL;
+
+public class CardButton{
+	private static Card myCard;
+	private static URL imageURL;
+	private static final String directory = "/data/images/cards/";
+	
+	public static void main (String args[]) {
+	  Deck myDeck = new Deck();
+	  myCard = myDeck.dealCard();
+	  System.out.println(directory + myCard.getFilename(myCard.getSuit(), myCard.getRank()) );
+	  
+	  imageURL = findResource(directory + myCard.getFilename(myCard.getSuit(), myCard.getRank()));
+	  System.out.println(imageURL.getFile() );
+	}
+	
+}
