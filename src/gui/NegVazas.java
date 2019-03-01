9d6675831dc2947ecbf0f5ea4662513097654531
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-03-01 01:34:38 -0300

    [WIP] Refactor NegVazas

--- src/gui/NegVazas.java
+++ src/gui/NegVazas.java
@@ -1,9 +1,18 @@
 package gui;
 
-import core.*;
-import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JFrame;
+
+import core.Board;
+import core.Card;
+import core.Dealer;
+import core.Deck;
+import core.Direction;
+import core.Hand;
+
+@SuppressWarnings("serial")
 public class NegVazas extends JFrame {
 
 	// Constants
@@ -13,8 +22,6 @@ public class NegVazas extends JFrame {
 	private final int BETWEEN_CARDS_WIDTH = 26; /* 26 is good. 12 pixels to hide pictures */
 	private final int WIDTH = 1024;
 	private final int HEIGHT = 768;
-	private final int B_X = 10;
-	private final int B_Y = 600;
 	private final int NORTH_X = 1024 / 2 - BETWEEN_CARDS_WIDTH * 7; /* Ideal would be 6,5 (half of the cards) */
 	private final int SOUTH_X = NORTH_X;
 	private final int EAST_X = NORTH_X + 300;
@@ -31,25 +38,24 @@ public class NegVazas extends JFrame {
 	// Models
 
 	private Board board;
-
-	// Card and turn light positioning arrays
-
-	public int[] initial_x = new int[4];
-	public int[] initial_y = new int[4];
+	
+	
 	public int[] light_x = new int[4];
 	public int[] light_y = new int[4];
 
 	// Swing items
 
-	private JButton[] trickCardsButtons = new JButton[4];
-	private JButton[] trickButtons = new JButton[4];
-	private JButton[][] buttons = new JButton[NUMBER_OF_HANDS][SIZE_OF_HAND]; /* Don't know how not to use this. */
-	private JButton[][] handButtons = new JButton[NUMBER_OF_HANDS][SIZE_OF_HAND];
+	private List<CardButton> trickCardButtons = new ArrayList<CardButton>();
+	private List<CardButton> northHandButtons = new ArrayList<CardButton>();
+	private List<CardButton> eastHandButtons = new ArrayList<CardButton>();
+	private List<CardButton> southHandButtons = new ArrayList<CardButton>();
+	private List<CardButton> westHandButtons = new ArrayList<CardButton>();
 
 	javax.swing.JButton drawNewHandButton = new javax.swing.JButton();
 	private int turn;
 	javax.swing.JButton turnLight = new javax.swing.JButton();
 	javax.swing.JButton scoreboardButton = new javax.swing.JButton();
+	private DeckOfCardButtons deckOfCardButtons;
 
 	public static void main(String args[]) {
 		NegVazas negVazas = new NegVazas();
@@ -60,74 +66,54 @@ public class NegVazas extends JFrame {
 
 	public void init() {
 
-		setInitialHandPositioning();
-
 		setInitialTurnLightPositioning();
 
 		initializeContentPane();
 
-		/* Cards */
-		for (int k = 0; k < NUMBER_OF_HANDS; k++) {
-			for (int i = SIZE_OF_HAND - 1; i >= 0; i--) { /* Only way I figured to paint in order */
-				buttons[k][i] = new JButton();
+		createDeckOfCardButtons();
+		displayDeckOfCardButtons();
 
-				buttons[k][i].setFocusPainted(false);
-				buttons[k][i].setRolloverEnabled(false); /* Does not bring up the focused button */
-				buttons[k][i].setBorderPainted(false); /* Does not paint the border */
-				buttons[k][i].setContentAreaFilled(false); /* Paint always in the same order??? */
-
-				buttons[k][i].setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-				buttons[k][i].setToolTipText("This is a card.");
-				getContentPane().add(buttons[k][i]);
-				buttons[k][i].setBounds(initial_x[k] + i * BETWEEN_CARDS_WIDTH, initial_y[k], CARD_WIDTH, CARD_HEIGHT);
-				// JLabel array mapping
-				handButtons[k][i] = buttons[k][i]; /*
-													 * Really didn't understand the need of this line but break without
-													 * it
-													 */
-			}
-		}
 
 		/* Trick */
-		int center_x = WIDTH / 2;
-		int center_y = HEIGHT / 2;
-		int[] trick_x = new int[4];
-		int[] trick_y = new int[4];
-		trick_x[0] = trick_x[2] = center_x - CARD_WIDTH / 2;
-		trick_x[1] = center_x + 30 - CARD_WIDTH / 2;
-		trick_x[3] = center_x - 30 - CARD_WIDTH / 2;
-
-		trick_y[1] = trick_y[3] = center_y - CARD_HEIGHT / 2;
-		trick_y[0] = center_y - 30 - CARD_HEIGHT / 2;
-		trick_y[2] = center_y + 30 - CARD_HEIGHT / 2;
-
-		for (int i = 0; i < 4; i++)
-			trick_y[i] -= 40;
-
-		for (int i = 0; i < 4; i++) {
-			trickCardsButtons[i] = new JButton();
-
-			trickCardsButtons[i].setFocusPainted(false);
-			trickCardsButtons[i].setRolloverEnabled(false); /* Does not bring up the focused button */
-			trickCardsButtons[i].setBorderPainted(false); /* Does not paint the border */
-			trickCardsButtons[i].setContentAreaFilled(false); /* Paint always in the same order??? */
-
-			trickCardsButtons[i].setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-			trickCardsButtons[i].setToolTipText("This is a card.");
-			getContentPane().add(trickCardsButtons[i]);
-			trickCardsButtons[i].setBounds(trick_x[i], trick_y[i], CARD_WIDTH, CARD_HEIGHT);
-			// JLabel array mapping
-			trickButtons[i] = trickCardsButtons[i]; /*
-													 * Really didn't understand the need of this line but break without
-													 * it
-													 */
-		}
+//		int center_x = WIDTH / 2;
+//		int center_y = HEIGHT / 2;
+//		int[] trick_x = new int[4];
+//		int[] trick_y = new int[4];
+//		trick_x[0] = trick_x[2] = center_x - CARD_WIDTH / 2;
+//		trick_x[1] = center_x + 30 - CARD_WIDTH / 2;
+//		trick_x[3] = center_x - 30 - CARD_WIDTH / 2;
+//
+//		trick_y[1] = trick_y[3] = center_y - CARD_HEIGHT / 2;
+//		trick_y[0] = center_y - 30 - CARD_HEIGHT / 2;
+//		trick_y[2] = center_y + 30 - CARD_HEIGHT / 2;
+//
+//		for (int i = 0; i < 4; i++)
+//			trick_y[i] -= 40;
+//
+//		for (int i = 0; i < 4; i++) {
+//			trickCardsButtons[i] = new JButton();
+//
+//			trickCardsButtons[i].setFocusPainted(false);
+//			trickCardsButtons[i].setRolloverEnabled(false); /* Does not bring up the focused button */
+//			trickCardsButtons[i].setBorderPainted(false); /* Does not paint the border */
+//			trickCardsButtons[i].setContentAreaFilled(false); /* Paint always in the same order??? */
+//
+//			trickCardsButtons[i].setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+//			trickCardsButtons[i].setToolTipText("This is a card.");
+//			getContentPane().add(trickCardsButtons[i]);
+//			trickCardsButtons[i].setBounds(trick_x[i], trick_y[i], CARD_WIDTH, CARD_HEIGHT);
+//			// JLabel array mapping
+//			trickButtons[i] = trickCardsButtons[i]; /*
+//													 * Really didn't understand the need of this line but break without
+//													 * it
+//													 */
+//		}
 
 		initializeBoard();
-		
+
 		turn = board.getCurrentPlayer().toInt();
-		turnLight.setBounds(initial_x[turn] - 10, initial_y[turn] - 10,
-				(SIZE_OF_HAND - 1) * BETWEEN_CARDS_WIDTH + CARD_WIDTH + 10 + 10, CARD_HEIGHT + 20);
+//		turnLight.setBounds(initial_x[turn] - 10, initial_y[turn] - 10,
+//				(SIZE_OF_HAND - 1) * BETWEEN_CARDS_WIDTH + CARD_WIDTH + 10 + 10, CARD_HEIGHT + 20);
 		// turn_light.setBorderPainted(true);
 		turnLight.setRolloverEnabled(false);
 		turnLight.setBackground(TURN_LIGHT_COLOR);
@@ -145,31 +131,21 @@ public class NegVazas extends JFrame {
 		getContentPane().add(scoreboardButton);
 		scoreboardButton.setBounds(WIDTH - 150, 10, 120, 120);
 
-		
-		display();
+		displayHands();
 
 		/* Listeners */
 
-		DrawAndSortActionListener drawNewHandActionListener = new DrawAndSortActionListener();
-		PlayCardActionListener playCardActionListener = new PlayCardActionListener();
-		drawNewHandButton.addActionListener(drawNewHandActionListener);
-		for (int k = 0; k < NUMBER_OF_HANDS; k++) {
-			for (int i = 0; i < SIZE_OF_HAND; i++) {
-				handButtons[k][i].addActionListener(playCardActionListener);
-			}
-		}
+//		DrawAndSortActionListener drawNewHandActionListener = new DrawAndSortActionListener();
+//		PlayCardActionListener playCardActionListener = new PlayCardActionListener();
+//		drawNewHandButton.addActionListener(drawNewHandActionListener);
+//		for (int k = 0; k < NUMBER_OF_HANDS; k++) {
+//			for (int i = 0; i < SIZE_OF_HAND; i++) {
+//				handButtons[k][i].addActionListener(playCardActionListener);
+//			}
+//		}
 	}
 
-	private void setInitialHandPositioning() {
-		initial_y[0] = NORTH_Y;
-		initial_y[1] = EAST_Y;
-		initial_y[2] = SOUTH_Y;
-		initial_y[3] = WEST_Y;
-		initial_x[0] = NORTH_X;
-		initial_x[1] = EAST_X;
-		initial_x[2] = SOUTH_X;
-		initial_x[3] = WEST_X;
-	}
+	
 
 	private void setInitialTurnLightPositioning() {
 		light_y[0] = NORTH_Y;
@@ -194,63 +170,63 @@ public class NegVazas extends JFrame {
 	private void initializeBoard() {
 		Dealer dealer = new Dealer();
 		board = dealer.deal();
-		Hand currentHand = board.getNorthHand();
-	}
-
-	private boolean isValid(Card c, Hand h) {
-		return board.isValid(c, h);
-	}
-
-	private void draw() {
-		initializeBoard();
-		restoreButtons();
-	}
-
-	private void playCard(Card card) {
-		board.playCard(card);
 	}
 
 	private void restoreButtons() {
-		for (int k = 0; k < NUMBER_OF_HANDS; k++)
-			for (int i = 0; i < SIZE_OF_HAND; i++) {
-				handButtons[k][i].setVisible(true);
-			}
-		for (int i = 0; i < 4; i++) {
-			trickButtons[i].setVisible(false);
-		}
-		scoreboardButton.setText("NS:" + board.getNorthSouthTricks() + " EW:" + board.getEastWestTricks());
+//		for (int k = 0; k < NUMBER_OF_HANDS; k++)
+//			for (int i = 0; i < SIZE_OF_HAND; i++) {
+//				handButtons[k][i].setVisible(true);
+//			}
+//		for (int i = 0; i < 4; i++) {
+//			trickButtons[i].setVisible(false);
+//		}
+//		scoreboardButton.setText("NS:" + board.getNorthSouthTricks() + " EW:" + board.getEastWestTricks());
 	}
 
-	private void display() {
+	private void refreshScreen() {
+		displayHands();
+		displayTrick();
+		displayTurnLight();
+	}
+
+	private void createDeckOfCardButtons() {	
+		Deck deck = new Deck();
+		deck.shuffle();
+		this.deckOfCardButtons = new DeckOfCardButtons(deck, new PlayCardActionListener());
+	}
+
+	private void displayDeckOfCardButtons() {
+		this.deckOfCardButtons.addAllCardButtonsToPane(this.getContentPane());
+		
+	}
+	private void displayHands() {
 		int numberOfCards;
 		int discarded; /* Number of cards already discarded */
 		Hand currentHand;
 		for (int k = 0; k < NUMBER_OF_HANDS; k++) {
-			if(k==0) {
+			if (k == 0) {
 				currentHand = board.getNorthHand();
-			}
-			else if(k==1) {
+			} else if (k == 1) {
 				currentHand = board.getEastHand();
-			}
-			else if(k==2) {
+			} else if (k == 2) {
 				currentHand = board.getSouthHand();
-			}
-			else if(k==3) {
+			} else if (k == 3) {
 				currentHand = board.getWestHand();
-			}else {
+			} else {
 				throw new RuntimeException("Invalid hand");
 			}
+
 			numberOfCards = currentHand.getNumberOfCards();
 			discarded = SIZE_OF_HAND - numberOfCards;
-			for (int i = 0; i < numberOfCards; i++) {
-				Card c = currentHand.getCard(i);
-				handButtons[k][i].setIcon(c.getCardImage());
-				handButtons[k][i].setBounds(
-						initial_x[k] + i * BETWEEN_CARDS_WIDTH + discarded * BETWEEN_CARDS_WIDTH / 2, initial_y[k],
-						CARD_WIDTH, CARD_HEIGHT);
-			}
+//			for (int i = 0; i < numberOfCards; i++) {
+//				Card c = currentHand.getCard(i);
+//				handButtons[k][i].setIcon(c.getCardImage());
+//				handButtons[k][i].setBounds(
+//						initial_x[k] + i * BETWEEN_CARDS_WIDTH + discarded * BETWEEN_CARDS_WIDTH / 2, initial_y[k],
+//						CARD_WIDTH, CARD_HEIGHT);
+//			}
+
 		}
-		displayTrick();
 	}
 
 	private void displayTrick() {
@@ -264,24 +240,18 @@ public class NegVazas extends JFrame {
 				diff -= 4;
 		}
 		for (int i = 0; i < 4; i++) {
-			trickButtons[i].setVisible(false);
+//			trickButtons[i].setVisible(false);
 		}
 		for (int i = 0; i < cards; i++) {
 			Card c = board.getCurrentTrick().getCard(i);
-			trickButtons[(diff + i) % 4].setIcon(c.getCardImage());
-			trickButtons[(diff + i) % 4].setVisible(true);
-			// System.out.println(myTrick.getCard(i).toString());
+//			trickButtons[(diff + i) % 4].setIcon(c.getCardImage());
+//			trickButtons[(diff + i) % 4].setVisible(true);
 		}
-//	  System.out.println();
 	}
 
-	private void moveTurnLight() {
-		turnLight.setBounds(light_x[turn] - 10, light_y[turn] - 10, 20, 20);
-
-	}
-
-	private void displayTurn() {
-//		int cards = hands[turn].getNumberOfCards();
+	private void displayTurnLight() {
+//		Hand handOfCurrentPlayer = board.getHandOfCurrentPlayer();
+//		int cards = handOfCurrentPlayer.getNumberOfCards();
 //		int discarded = SIZE_OF_HAND - cards;
 //		turnLight.setBounds(initial_x[turn] - 10 + discarded * BETWEEN_CARDS_WIDTH / 2, initial_y[turn] - 10,
 //				(cards - 1) * BETWEEN_CARDS_WIDTH + CARD_WIDTH + 10 + 10, CARD_HEIGHT + 20);
@@ -289,27 +259,8 @@ public class NegVazas extends JFrame {
 
 	class PlayCardActionListener implements java.awt.event.ActionListener {
 		public void actionPerformed(java.awt.event.ActionEvent event) {
-			Object object = event.getSource();
-			for (int k = 0; k < NUMBER_OF_HANDS; k++) {
-				for (int i = 0; i < SIZE_OF_HAND; i++) {
-					if (k == turn && object == handButtons[k][i]) {
-						//Card c = hands[k].getCard(i);
-						//playCard(c);
-
-					}
-				}
-			}
-			moveTurnLight();
-			displayTurn();
-			display();
-		}
-	}
-
-	class DrawAndSortActionListener implements java.awt.event.ActionListener {
-		public void actionPerformed(java.awt.event.ActionEvent event) {
-			draw();
-			displayTurn();
-			display();
+			CardButton clickedCardButton = (CardButton) event.getSource();
+			clickedCardButton.flip();
 		}
 	}
 
