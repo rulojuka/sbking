7da2eaaab3b85792ed6ec6f8ef4225c38c0a3b5b
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-04-06 20:22:12 -0300

    Refactor exceptions

--- src/main/java/br/com/sbk/sbking/core/exceptions/DealNotFinishedException.java
+++ src/main/java/br/com/sbk/sbking/core/exceptions/DealNotFinishedException.java
@@ -3,4 +3,8 @@ package br.com.sbk.sbking.core.exceptions;
 @SuppressWarnings("serial")
 public class DealNotFinishedException extends RuntimeException {
 
+	public DealNotFinishedException() {
+		super("The deal is not finished yet.");
+	}
+	
 }
