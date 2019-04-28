1dd9b76e8d0398071f8b52b891d3fbb3d3c8e34e
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-04-28 06:04:57 -0300

    Remove unnecessary 'symbol' from Strain.java

--- src/main/java/br/com/sbk/sbking/core/Strain.java
+++ src/main/java/br/com/sbk/sbking/core/Strain.java
@@ -5,28 +5,22 @@ import br.com.sbk.sbking.core.rulesets.concrete.PositiveNoTrumpsRuleset;
 import br.com.sbk.sbking.core.rulesets.concrete.PositiveWithTrumpsRuleset;
 
 public enum Strain {
-	DIAMONDS("Diamonds", "d", new PositiveWithTrumpsRuleset(Suit.DIAMONDS)),
-	CLUBS("Clubs", "c", new PositiveWithTrumpsRuleset(Suit.CLUBS)),
-	HEARTS("Hearts", "h", new PositiveWithTrumpsRuleset(Suit.HEARTS)),
-	SPADES("Spades", "s", new PositiveWithTrumpsRuleset(Suit.SPADES)),
-	NOTRUMPS("No Trumps", "n", new PositiveNoTrumpsRuleset());
+	DIAMONDS("Diamonds", new PositiveWithTrumpsRuleset(Suit.DIAMONDS)),
+	CLUBS("Clubs", new PositiveWithTrumpsRuleset(Suit.CLUBS)),
+	HEARTS("Hearts", new PositiveWithTrumpsRuleset(Suit.HEARTS)),
+	SPADES("Spades", new PositiveWithTrumpsRuleset(Suit.SPADES)),
+	NOTRUMPS("No Trumps", new PositiveNoTrumpsRuleset());
 
 	private final String name;
-	private final String symbol;
 	private final PositiveRuleset positiveRuleset;
 
-	private Strain(String name, String symbol, PositiveRuleset positiveRuleset) {
+	private Strain(String name, PositiveRuleset positiveRuleset) {
 		this.name = name;
-		this.symbol = symbol;
 		this.positiveRuleset = positiveRuleset;
 	}
 
 	public String getName() {
 		return this.name;
-	}
-
-	public String getSymbol() {
-		return this.symbol;
 	}
 
 	public PositiveRuleset getPositiveRuleset() {
