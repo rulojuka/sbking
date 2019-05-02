fcaa40a9a058492544063a0a0207fe8dfa6ecb0d
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2019-05-01 22:27:52 -0300

    Test Strain

--- src/main/java/br/com/sbk/sbking/core/rulesets/concrete/PositiveWithTrumpsRuleset.java
+++ src/main/java/br/com/sbk/sbking/core/rulesets/concrete/PositiveWithTrumpsRuleset.java
@@ -28,4 +28,26 @@ public class PositiveWithTrumpsRuleset extends PositiveRuleset {
 		return "Make the most tricks with " + trumpSuit.getName() + " as trump suit";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((trumpSuit == null) ? 0 : trumpSuit.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PositiveWithTrumpsRuleset other = (PositiveWithTrumpsRuleset) obj;
+		if (trumpSuit != other.trumpSuit)
+			return false;
+		return true;
+	}
+
 }
---
+++ src/test/java/br/com/sbk/sbking/core/StrainTest.java
@@ -0,0 +1,61 @@
+package br.com.sbk.sbking.core;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.com.sbk.sbking.core.rulesets.concrete.PositiveNoTrumpsRuleset;
+import br.com.sbk.sbking.core.rulesets.concrete.PositiveWithTrumpsRuleset;
+
+public class StrainTest {
+
+	private static Strain diamonds;
+	private static Strain clubs;
+	private static Strain hearts;
+	private static Strain spades;
+	private static Strain noTrumps;
+
+	@BeforeClass
+	public static void setup() {
+		diamonds = Strain.DIAMONDS;
+		clubs = Strain.CLUBS;
+		hearts = Strain.HEARTS;
+		spades = Strain.SPADES;
+		noTrumps = Strain.NOTRUMPS;
+	}
+
+	@Test
+	public void theSameStrainShouldAlwaysBeTheSameObject() {
+		Strain strain1 = Strain.CLUBS;
+		Strain strain2 = Strain.CLUBS;
+		assertTrue(strain1 == strain2);
+	}
+
+	@Test
+	public void theSameStrainShouldAlwaysBeEqual() {
+		Strain strain1 = Strain.CLUBS;
+		Strain strain2 = Strain.CLUBS;
+		assertEquals(strain1, strain2);
+	}
+
+	@Test
+	public void shouldGetName() {
+		assertEquals("Diamonds", diamonds.getName());
+		assertEquals("Clubs", clubs.getName());
+		assertEquals("Hearts", hearts.getName());
+		assertEquals("Spades", spades.getName());
+		assertEquals("No Trumps", noTrumps.getName());
+	}
+
+	@Test
+	public void shouldGetPositiveRuleset() {
+		assertEquals(new PositiveWithTrumpsRuleset(Suit.DIAMONDS), diamonds.getPositiveRuleset());
+		assertEquals(new PositiveWithTrumpsRuleset(Suit.CLUBS), clubs.getPositiveRuleset());
+		assertEquals(new PositiveWithTrumpsRuleset(Suit.HEARTS), hearts.getPositiveRuleset());
+		assertEquals(new PositiveWithTrumpsRuleset(Suit.SPADES), spades.getPositiveRuleset());
+		assertEquals(new PositiveNoTrumpsRuleset(), noTrumps.getPositiveRuleset());
+	}
+
+}
