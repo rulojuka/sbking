0a6ce0024aba12ff114cd9c66637ae5577a47010
Guilherme Fernandes <aateg@live.com>
2021-03-10 16:56:25 -0300

    [chore] add FourteenHCP... test

--- src/test/java/br/com/sbk/sbking/core/BoardDealerTest.java
+++ src/test/java/br/com/sbk/sbking/core/BoardDealerTest.java
@@ -2,6 +2,7 @@ package br.com.sbk.sbking.core;
 
 import static br.com.sbk.sbking.core.GameConstants.SIZE_OF_HAND;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -14,14 +15,33 @@ public class BoardDealerTest {
 		BoardDealer boardDealer = new ShuffledBoardDealer();
 		Board board = boardDealer.dealBoard(dealer);
 
-		// The correct test should verify if new Board(completeHand1, completeHand2,
-		// completeHand3, completeHand4, dealer) was called but Mockito can't do that.
+		// The correct test should verify if new Board(hands, dealer) 
+		// was called but Mockito can't do that.
 		// Coupling this test with Hand and Board instead :(
 		for (Direction direction : Direction.values()) {
 			assertEquals(SIZE_OF_HAND, board.getHandOf(direction).size());
 		}
 		assertEquals(dealer, board.getDealer());
 
+	}
+
+	@Test
+	public void shouldReceiveABoardWithTheCorrectDealerAndACompleteSetOfCardsAnd14HCPPlusCardsAndShortestSuit2Plus() {
+		Direction dealer = Direction.NORTH;
+		BoardDealer boardDealer = new FourteenHCPPlusDoubletonRuledBoardDealer();
+		Board board = boardDealer.dealBoard(dealer);
+
+		// Verifying if all hands are complete
+		for (Direction direction : Direction.values()) {
+			assertEquals(SIZE_OF_HAND, board.getHandOf(direction).size());
+		}
+		// Verifying if it's the correct dealer
+		assertEquals(dealer, board.getDealer());
+
+		// Verifying hands condition: 14 HCP minimum and doubleton ruled
+		assertTrue(board.getHandOf(dealer.getPositiveOrNegativeChooserWhenDealer()).getHCP() >= 14);
+		assertTrue(board.getHandOf(dealer.getPositiveOrNegativeChooserWhenDealer()).getShortestSuitLength() >= 2);
+		
 	}
 
 }
