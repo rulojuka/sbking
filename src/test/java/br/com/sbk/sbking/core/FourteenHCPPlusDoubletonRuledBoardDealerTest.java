d34cba2af9adbf9dad63a2400df5cafe6d1ae73e
Guilherme Fernandes <aateg@live.com>
2021-03-11 16:01:11 -0300

    [refact] shuffled and ruled (14HCP...) board dealer test

---
+++ src/test/java/br/com/sbk/sbking/core/FourteenHCPPlusDoubletonRuledBoardDealerTest.java
@@ -0,0 +1,33 @@
+package br.com.sbk.sbking.core;
+
+import static br.com.sbk.sbking.core.GameConstants.SIZE_OF_HAND;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class FourteenHCPPlusDoubletonRuledBoardDealerTest {
+
+	// FIXME This is an integration test as it needs other classes to work.
+	@Test
+	public void shouldReceiveABoardWithTheCorrectDealerAndACompleteSetOfCardsAnd14HCPPlusCardsAndShortestSuit2Plus() {
+		Direction dealer = Direction.NORTH;
+		BoardDealer boardDealer = new FourteenHCPPlusDoubletonRuledBoardDealer();
+		Board board = boardDealer.dealBoard(dealer);
+
+		for (Direction direction : Direction.values()) {
+			assertEquals(SIZE_OF_HAND, board.getHandOf(direction).size());
+		}
+    
+		assertEquals(dealer, board.getDealer());
+
+		this.validateHand(board.getHandOf(dealer.getPositiveOrNegativeChooserWhenDealer()));
+		
+	}
+
+    private void validateHand(Hand hand) {
+        assertTrue(hand.getHCP() >= 14);
+		assertTrue(hand.getShortestSuitLength() >= 2);
+    }
+
+}
