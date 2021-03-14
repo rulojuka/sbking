a849687a7a82f581186181ce4591569e9d2c5513
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2021-03-13 23:17:41 -0300

    Moving hard coded constraint from Hand to DeckInfo

--- src/main/java/br/com/sbk/sbking/gui/models/DeckCardImageInformation.java
+++ src/main/java/br/com/sbk/sbking/gui/models/DeckCardImageInformation.java
@@ -10,16 +10,24 @@ import br.com.sbk.sbking.core.Card;
 import br.com.sbk.sbking.core.Rank;
 import br.com.sbk.sbking.core.Suit;
 
+import br.com.sbk.sbking.gui.constants.FrameConstants;
+
 
 public class DeckCardImageInformation {
 	private static final String directory = "/images/cards/";
 
-	private static final int CARD_IMAGE_WIDTH = 182;
-	private static final int CARD_IMAGE_HEIGHT = 247; 
-	private static final double SCALE_FACTOR = 0.75;
+	private static final int ORIGINAL_CARD_IMAGE_WIDTH = 182;
+	private static final int ORIGINAL_CARD_IMAGE_HEIGHT = 247; 
+	private static final int ORIGINAL_WIDTH_BETWEEN_CARDS = 48;
 
-	private static final int CARD_WIDTH = (int)(CARD_IMAGE_WIDTH*SCALE_FACTOR);
-	private static final int CARD_HEIGHT = (int)(CARD_IMAGE_HEIGHT*SCALE_FACTOR);
+	private static double SCALE_FACTOR;
+	private static int CARD_WIDTH;
+	private static int CARD_HEIGHT;
+	private static int WIDTH_BETWEEN_CARDS;
+
+	public DeckCardImageInformation() {
+		computeDeckCardImageInformation();
+	}
 
 	public ImageIcon getBackImage() {
 		String imagePath = directory + "back.png";
@@ -46,8 +54,19 @@ public class DeckCardImageInformation {
 		return CARD_HEIGHT;
 	}
 
+	public int getWidthBetweenCards() {
+		return WIDTH_BETWEEN_CARDS;
+	}
+
 	private ImageIcon getScaledCardImage(URL url) {
 		Image cardImage = new ImageIcon(url).getImage();
 		return new ImageIcon(cardImage.getScaledInstance(CARD_WIDTH, CARD_HEIGHT, java.awt.Image.SCALE_SMOOTH));
+	}
+
+	public void computeDeckCardImageInformation() {
+		SCALE_FACTOR = FrameConstants.getScreenScale();
+		CARD_WIDTH = (int)(ORIGINAL_CARD_IMAGE_WIDTH*SCALE_FACTOR);
+		CARD_HEIGHT = (int)(ORIGINAL_CARD_IMAGE_HEIGHT*SCALE_FACTOR);
+		WIDTH_BETWEEN_CARDS = (int)(ORIGINAL_WIDTH_BETWEEN_CARDS*SCALE_FACTOR);
 	}
 }
