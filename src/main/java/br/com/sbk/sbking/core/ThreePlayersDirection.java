bfd4cc03e0ac13ef7906ecf690ba93e5c5968a12
Guilherme Fernandes <aateg@live.com>
2021-03-18 16:46:56 -0300

    [feat] add class ThreePlayersDirection

---
+++ src/main/java/br/com/sbk/sbking/core/ThreePlayersDirection.java
@@ -0,0 +1,46 @@
+package br.com.sbk.sbking.core;
+
+public enum ThreePlayersDirection {
+
+    FOREHAND("Forehand", 'F'), MIDDLEHAND("Middlehand", 'M'), HINDHAND("Hindhand", 'H');
+
+    private final String completeName;
+	private final char abbreviation;
+
+    private ThreePlayersDirection(String completeName, char abbreviation) {
+        this.completeName = completeName;
+        this.abbreviation = abbreviation;
+    }
+
+    // Static copy to avoid many copies
+	private static ThreePlayersDirection[] vals = values();
+
+    public ThreePlayersDirection next(int n) {
+		return vals[(this.ordinal() + n) % vals.length];
+	}
+
+	public ThreePlayersDirection next() {
+		return this.next(1);
+	}
+
+	public boolean isForehand() {
+		return this == ThreePlayersDirection.FOREHAND;
+	}
+
+	public boolean isMiddlehand() {
+		return this == ThreePlayersDirection.MIDDLEHAND;
+	}
+
+	public boolean isHindhand() {
+		return this == ThreePlayersDirection.HINDHAND;
+	}
+
+    public String getCompleteName() {
+		return completeName;
+	}
+
+	public char getAbbreviation() {
+		return abbreviation;
+	}
+    
+}
