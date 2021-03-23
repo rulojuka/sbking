48e7fce938dd6fc2204d371fbbd6298c2781513b
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2021-03-23 20:16:35 -0300

    [fix] converting tabs to spaces

---
+++ src/main/java/br/com/sbk/sbking/networking/messages/MessageConstants.java
@@ -0,0 +1,23 @@
+package br.com.sbk.sbking.networking.messages;
+
+public final class MessageConstants {
+
+    public static final String MESSAGE = "MESSAGE";
+    public static final String DEAL = "DEAL";
+    public static final String BOARD = "BOARD";
+    public static final String INITIALIZEDEAL = "INITIALIZEDEAL";
+    public static final String FINISHDEAL = "FINISHDEAL";
+    public static final String DIRECTION = "DIRECTION";
+    public static final String WAIT = "WAIT";
+    public static final String CONTINUE = "CONTINUE";
+    public static final String CHOOSERPOSITIVENEGATIVE = "CHOOSERPOSITIVENEGATIVE";
+    public static final String CHOOSERGAMEMODEORSTRAIN = "CHOOSERGAMEMODEORSTRAIN";
+    public static final String POSITIVEORNEGATIVE = "POSITIVEORNEGATIVE";
+    public static final String GAMEMODEORSTRAIN = "GAMEMODEORSTRAIN";
+    public static final String FINISHGAME = "FINISHGAME";
+    public static final String GAMESCOREBOARD = "GAMESCOREBOARD";
+    public static final String INVALIDRULESET = "INVALIDRULESET";
+    public static final String VALIDRULESET = "VALIDRULESET";
+    public static final String ISSPECTATOR = "ISSPECTATOR";
+    public static final String ISNOTSPECTATOR = "ISNOTSPECTATOR";
+}
