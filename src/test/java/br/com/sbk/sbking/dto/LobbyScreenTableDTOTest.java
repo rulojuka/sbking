26fd8e1339a1ae4c76c6005b7d3202acd4811de6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2021-04-18 20:23:12 -0300

    [feat] GetTablesMessage

--- src/test/java/br/com/sbk/sbking/dto/LobbyScreenTableDTOTest.java
+++ src/test/java/br/com/sbk/sbking/dto/LobbyScreenTableDTOTest.java
@@ -1,44 +1,61 @@
 package br.com.sbk.sbking.dto;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Map;
+import java.util.UUID;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
 import br.com.sbk.sbking.core.Direction;
 import br.com.sbk.sbking.core.Player;
-import br.com.sbk.sbking.core.TrickGame;
+import br.com.sbk.sbking.networking.server.Table;
 
 public class LobbyScreenTableDTOTest {
 
     @Mock
-    Map<Direction, Player> playersDirection;
+    private UUID tableId;
     @Mock
-    TrickGame trickGame;
+    private Map<Direction, Player> playersDirections;
     @Mock
-    int numberOfSpectators;
+    private String gameName;
+    @Mock
+    private int numberOfSpectators;
+
+    LobbyScreenTableDTO subject;
+
+    @Before
+    public void setupMocks() {
+        Table table = mock(Table.class);
+        when(table.getId()).thenReturn(tableId);
+        when(table.getPlayersDirections()).thenReturn(playersDirections);
+        when(table.getGameName()).thenReturn(gameName);
+        when(table.getNumberOfSpectators()).thenReturn(numberOfSpectators);
+        subject = new LobbyScreenTableDTO(table);
+    }
+
+    @Test
+    public void getIdDirectionShouldReturnTableId() {
+        assertEquals(tableId, subject.getId());
+    }
 
     @Test
     public void getPlayersDirectionShouldReturnPlayersDirection() {
-        LobbyScreenTableDTO dto = new LobbyScreenTableDTO(playersDirection, trickGame, numberOfSpectators);
-
-        assertEquals(playersDirection, dto.getPlayersDirection());
+        assertEquals(playersDirections, subject.getPlayersDirection());
     }
 
     @Test
     public void getNumberOfSpectatorsShouldReturnNumberOfSpectators() {
-        LobbyScreenTableDTO dto = new LobbyScreenTableDTO(playersDirection, trickGame, numberOfSpectators);
-
-        assertEquals(numberOfSpectators, dto.getNumberOfSpectators());
+        assertEquals(numberOfSpectators, subject.getNumberOfSpectators());
     }
 
     @Test
-    public void getGameScreenShouldReturnTheGameScreen() {
-        LobbyScreenTableDTO dto = new LobbyScreenTableDTO(playersDirection, trickGame, numberOfSpectators);
-
-        assertEquals(trickGame, dto.getGameScreen());
+    public void getGameNameShouldReturnTheGameName() {
+        assertEquals(gameName, subject.getGameName());
     }
 
 }
