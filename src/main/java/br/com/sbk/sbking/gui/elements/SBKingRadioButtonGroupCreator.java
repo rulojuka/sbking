b68ac0b61c43aa39b2acb691d3e31aea7aec9389
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2021-07-28 17:20:27 -0300

    [refactor] add color to radio button texts

--- src/main/java/br/com/sbk/sbking/gui/elements/SBKingRadioButtonGroupCreator.java
+++ src/main/java/br/com/sbk/sbking/gui/elements/SBKingRadioButtonGroupCreator.java
@@ -5,17 +5,21 @@ import java.util.List;
 import javax.swing.ButtonGroup;
 import javax.swing.JRadioButton;
 
+import br.com.sbk.sbking.gui.models.TextWithColor;
+
 public class SBKingRadioButtonGroupCreator {
 
   private static int elementHeight = 20;
 
-  public ButtonGroup create(List<String> texts, int x, int y, int elementWidth) {
+  public ButtonGroup create(List<TextWithColor> texts, int x, int y, int elementWidth) {
     ButtonGroup buttonGroup = new ButtonGroup();
 
-    for (String text : texts) {
+    for (TextWithColor textWithColor : texts) {
+      String text = textWithColor.getText();
       JRadioButton currentButton = new JRadioButton(text);
       currentButton.setName(text);
       currentButton.setBounds(x, y, elementWidth, elementHeight);
+      currentButton.setForeground(textWithColor.getColor());
       y += elementHeight;
 
       buttonGroup.add(currentButton);
