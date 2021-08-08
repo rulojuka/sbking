201fcb352cb797b8459b39aa1fc3306de93591a5
Guilherme Fernandes <aateg@live.com>
2021-08-08 20:09:29 -0300

    [chore] responsiveness of texts GUI

--- src/main/java/br/com/sbk/sbking/gui/jelements/SitOrLeaveButton.java
+++ src/main/java/br/com/sbk/sbking/gui/jelements/SitOrLeaveButton.java
@@ -1,15 +1,25 @@
 package br.com.sbk.sbking.gui.jelements;
 
 import br.com.sbk.sbking.core.Direction;
+import br.com.sbk.sbking.gui.constants.FrameConstants;
 
 @SuppressWarnings("serial")
 public class SitOrLeaveButton extends SBKingButton {
+
+  private static final int DEFAULT_WIDTH = 250;
+  private static final int DEFAULT_HEIGHT = 30;
+  private static double scaleFactor;
+  private static int width;
+  private static int height;
 
   public SitOrLeaveButton(Direction direction) {
     super();
     this.putClientProperty("type", "SITORLEAVE");
     this.putClientProperty("direction", direction);
-    this.setSize(200, 15);
+    width = (int) (DEFAULT_WIDTH * scaleFactor);
+    height = (int) (DEFAULT_HEIGHT * scaleFactor);
+    scaleFactor = FrameConstants.getScreenScale();
+    this.setSize(width, height);
   }
 
 }
