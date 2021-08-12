101ade5cdbfd0c657323ad20f33f42896141150c
Guilherme Fernandes <aateg@live.com>
2021-08-12 15:34:41 -0300

    [fix] table cards alignment

--- src/main/java/br/com/sbk/sbking/gui/elements/AllTableCardsElement.java
+++ src/main/java/br/com/sbk/sbking/gui/elements/AllTableCardsElement.java
@@ -1,5 +1,8 @@
 package br.com.sbk.sbking.gui.elements;
 
+import static br.com.sbk.sbking.gui.constants.FrameConstants.tableWidth;
+import static br.com.sbk.sbking.gui.constants.FrameConstants.tableHeight;
+
 import java.awt.event.ActionListener;
 import java.awt.Container;
 import java.awt.Point;
@@ -10,7 +13,8 @@ import br.com.sbk.sbking.dto.LobbyScreenTableDTO;
 public class AllTableCardsElement {
 
   public AllTableCardsElement(Container container, List<LobbyScreenTableDTO> tables, ActionListener actionListener) {
-    Point position = new Point(0, 0);
+
+    Point position = new Point(tableWidth / 4, tableHeight / 5);
     for (LobbyScreenTableDTO table : tables) {
       new TableCardElement(container, table, (Point) position.clone(), actionListener);
       position.translate(200, 0);
