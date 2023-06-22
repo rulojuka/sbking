a52e509be5b1ae0517aa16ec4cb5dc2fb2f28957
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2023-06-22 09:36:04 +0200

    [feat] remove html/javascript client used for testing

--- src/main/java/br/com/sbk/sbking/clientapp/MyStompSessionHandler.java
+++ src/main/java/br/com/sbk/sbking/clientapp/MyStompSessionHandler.java
@@ -2,15 +2,12 @@ package br.com.sbk.sbking.clientapp;
 
 import static br.com.sbk.sbking.logging.SBKingLogger.LOGGER;
 
-import java.lang.reflect.Type;
-
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 
 import br.com.sbk.sbking.networking.client.SBKingClient;
-import br.com.sbk.sbking.networking.websockets.Greeting;
 
 public class MyStompSessionHandler extends StompSessionHandlerAdapter {
 
@@ -24,8 +21,7 @@ public class MyStompSessionHandler extends StompSessionHandlerAdapter {
     @Override
     public void afterConnected(StompSession session, StompHeaders connectedHeaders) {
         LOGGER.info("New session established : " + session.getSessionId());
-        session.subscribe("/topic/greetings", this);
-        LOGGER.info("Subscribed to /topic/greetings");
+
         session.subscribe("/topic/players", new PlayerListDTOFrameHandler(sbKingClient));
         LOGGER.info("Subscribed to /topic/players");
     }
@@ -34,17 +30,6 @@ public class MyStompSessionHandler extends StompSessionHandlerAdapter {
     public void handleException(StompSession session, StompCommand command, StompHeaders headers, byte[] payload,
             Throwable exception) {
         LOGGER.error("Got an exception", exception);
-    }
-
-    @Override
-    public Type getPayloadType(StompHeaders headers) {
-        return Greeting.class;
-    }
-
-    @Override
-    public void handleFrame(StompHeaders headers, Object payload) {
-        Greeting greeting = (Greeting) payload;
-        LOGGER.info("Received message: --" + greeting.getContent() + "--");
     }
 
 }
