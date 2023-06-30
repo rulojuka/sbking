81938e29ae8088c258380dc41069ce71860e7ab6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2023-06-30 22:30:08 +0200

    [refactor] change static fields into instance fields

--- src/main/java/br/com/sbk/sbking/gui/models/DeckCardImageInformation.java
+++ src/main/java/br/com/sbk/sbking/gui/models/DeckCardImageInformation.java
@@ -1,18 +1,15 @@
 package br.com.sbk.sbking.gui.models;
 
-import java.net.URL;
-
 import java.awt.Image;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 
 import br.com.sbk.sbking.core.Card;
 import br.com.sbk.sbking.core.Rank;
 import br.com.sbk.sbking.core.Suit;
-
 import br.com.sbk.sbking.gui.constants.FrameConstants;
 import br.com.sbk.sbking.gui.main.AssetLoader;
-
 
 public class DeckCardImageInformation {
     private static final String DIRECTORY = "/images/cards/";
@@ -21,13 +18,12 @@ public class DeckCardImageInformation {
     private static final int ORIGINAL_CARD_IMAGE_HEIGHT = 247;
     private static final int ORIGINAL_WIDTH_BETWEEN_CARDS = 48;
 
-    private static double scaleFactor;
-    private static int cardWidth;
-    private static int cardHeight;
-    private static int widthBetweenCards;
+    private int cardWidth;
+    private int cardHeight;
+    private int widthBetweenCards;
 
     public DeckCardImageInformation() {
-        scaleFactor = FrameConstants.getScreenScale();
+        double scaleFactor = FrameConstants.getScreenScale();
         cardWidth = (int) (ORIGINAL_CARD_IMAGE_WIDTH * scaleFactor);
         cardHeight = (int) (ORIGINAL_CARD_IMAGE_HEIGHT * scaleFactor);
         widthBetweenCards = (int) (ORIGINAL_WIDTH_BETWEEN_CARDS * scaleFactor);
