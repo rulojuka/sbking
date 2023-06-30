b367f347b364530d5a4d126b6025cbd253d7d01a
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2023-06-30 19:38:18 +0200

    [refactor] remove dead code

--- src/main/java/br/com/sbk/sbking/networking/server/SBKingServerFactory.java
+++ src/main/java/br/com/sbk/sbking/networking/server/SBKingServerFactory.java
@@ -12,7 +12,6 @@ import br.com.sbk.sbking.app.PlayerController;
 import br.com.sbk.sbking.networking.kryonet.KryonetSBKingServer;
 import br.com.sbk.sbking.networking.kryonet.KryonetServerFactory;
 import br.com.sbk.sbking.networking.kryonet.KryonetServerListenerFactory;
-import br.com.sbk.sbking.networking.kryonet.SBKingServerMessageConsumer;
 import br.com.sbk.sbking.networking.kryonet.messages.SBKingMessageWithIdentifier;
 
 public class SBKingServerFactory {
@@ -23,9 +22,7 @@ public class SBKingServerFactory {
     KryonetSBKingServer server = KryonetServerFactory.getRegisteredServer(sbKingServer);
 
     Listener kryonetServerListener = KryonetServerListenerFactory.getServerListener(server, serverMessageQueue); // Producer
-    SBKingServerMessageConsumer sbKingServerMessageConsumer = new SBKingServerMessageConsumer(serverMessageQueue); // Consumer
 
-    new Thread(sbKingServerMessageConsumer, "msg-consumer").start();
     server.addListener(kryonetServerListener);
 
     try {
